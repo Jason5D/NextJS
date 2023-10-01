ce66cce09b5871d012dd90367f73ab368388edb4
Jason <jason5donoghue@gmail.com>
2023-10-01 10:16:12 +0100

    added first next page

---
+++ next-app/src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+// app/dashboard.page is the UI for the '/dashboard' URL
+export default function Page() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">    
+            <h1>Hello, Dashboard Page!</h1>
+        </main>
+)}
---
+++ next-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+// app/page is the UI for the '/' URL
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <h1>Hello, NextJS. This is the Home Page!</h1> 
+    </main>
+  )
+}
